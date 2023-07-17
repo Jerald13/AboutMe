26915ac3f4c4fe06f7b0d446d0b5fedc98a15483
JrealFun <jeraldlee2002@gmail.com>
2023-07-17 14:28:25 +0800

    test2

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -6,26 +6,43 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.example.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val doneBtn : Button = findViewById(R.id.done_button)
+//        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        doneBtn.setOnClickListener{
+
+//        val doneBtn : Button = findViewById(R.id.done_button)
+//
+//        doneBtn.setOnClickListener{
+//            addNickName(it)
+//        }
+
+
+        binding.doneButton.setOnClickListener{
             addNickName(it)
         }
+
 
     }
 
     private fun addNickName(view: View) {
-        val editText = findViewById<EditText>(R.id.nickname_edit)
-        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
+//        val editText = findViewById<EditText>(R.id.nickname_edit)
+//        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
+//
+//        nicknameTextView.text = editText.text
+//        editText.visibility = View.GONE
+//        view.visibility = View.GONE
+//        nicknameTextView.visibility = View.VISIBLE
 
-        nicknameTextView.text = editText.text
-        editText.visibility = View.GONE
-        view.visibility = View.GONE
-        nicknameTextView.visibility = View.VISIBLE
+        binding.nicknameText.text = binding.nicknameEdit.text
+        binding.nicknameEdit.visibility = View.GONE
+        binding.doneButton.visibility = View.GONE
+        binding.nicknameText.visibility = View.VISIBLE
     }
 }
